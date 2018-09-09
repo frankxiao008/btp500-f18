d61e1087046ab28239e36ba1b82a1a1ad53d73d6
Miguel Watler <miguel.watler@mtrx-node03pd.dcm.senecacollege.ca>
2018-09-09 16:25:44 -0400

    factorial, power and fibonacci functions

--- miguel/lab1.cpp
+++ miguel/lab1.cpp
@@ -1,10 +1,27 @@
 unsigned int factorial (unsigned int n){
-
+    int result=0;
+    for(int i=n; i>=1; --i) {
+        result *= n;
+    }
+    return result;
 }
 
 double power (double base, unsigned int n){
-
+    int result = 0;
+    for(int i=0; i<n; ++i) {
+        result *= n;
+    }
+    return result;
 }
+
 unsigned int fibonacci (unsigned int n){
-	
+    int Fib=1;
+    int FibPrev=0;
+    int result=0;
+    for(int i=0; i<n; ++i) {
+        result = Fib+FibPrev;
+        FibPrev = Fib;
+        Fib = result; 
+    }
+    return result;
 }
