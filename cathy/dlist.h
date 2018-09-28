dbdd06694d5af6f7bf1bc1a32c5adf15b12cb305
cathyatseneca <cathyatseneca@gmail.com>
2018-09-28 09:04:01 -0400

    dlist updated

--- cathy/dlist.h
+++ cathy/dlist.h
@@ -24,22 +24,88 @@ public:
 	void print() const;
 	void reversePrint() const;
 	~DList();
-
+	iterator begin(){
+		return iterator(front_,this);
+	}
+	iterator end(){
+		return iterator(nullptr,this);
+	}
+	const_iterator cbegin() const{
+		return const_iterator(front_,this);
+	}
+	const_iterator cend() const{
+		return const_iterator(nullptr,this);
+	}
 	class const_iterator{
+		friend class DList<T>;
+	protected:
 		Node* curr_;
+		DList* theList_;
+		const_iterator(Node* curr, DList* theList){
+			theList_=theList;
+			curr_=curr;
+		}
 	public:
-		const_iterator(){}
-		const_iterator operator++(){}
-		const_iterator operator++(int){}
-		const_iterator operator--(){}
-		const_iterator operator--(int){}
-		bool operator==(const_iterator rhs){}
-		bool operator!=(const_iterator rhs){}
-		const T& operator*()const{}
+		const_iterator(){
+			curr_=nullptr;
+			theList_=nullptr;
+		}
+		//++x
+		const_iterator operator++(){
+			curr_=curr_->next_;
+			return *this;
+		}
+
+		//x++
+		const_iterator operator++(int){
+			const_iterator old=*this;
+			curr_=curr_->next_;
+			return old;
+		}
+
+		//--x
+		const_iterator operator--(){
+			if(curr_!=nullptr){
+				curr_=curr_->prev_;
+			}
+			else{
+				if(theList_){
+					curr_=theList_->back_;
+				}
+			}
+			return *this;			
+		}
+
+		//x--
+		const_iterator operator--(int){
+			const_iterator old=*this;
+			curr_=curr_->prev_;
+			return old;			
+
+		}
+		bool operator==(const_iterator rhs){
+			bool rc=false;
+			if(theList_==rhs.theList_ && curr_==rhs.curr_){
+				rc=true;
+			}
+			return rc;
+		}
+		bool operator!=(const_iterator rhs){
+			return !(*this == rhs)
+		}
+		const T& operator*()const{
+			return curr_->data_;
+		}
 	};
 	class iterator:public const_iterator{
+		friend class DList<T>;		
+	protected:
+		iterator(Node* curr,DList* theList):const_iterator(curr,theList){}
 	public:
-		iterator();
+		iterator(){
+			this->theList_=nullptr;
+			this->curr_=nullptr;
+		}
 		iterator operator++(){}
 		iterator operator++(int){}
 		iterator operator--(){}
@@ -131,6 +197,18 @@ public:
 	void print() const;
 	void reversePrint() const;
 	~Sentinel();
+	iterator begin(){
+		return iterator(front_->next_,this);
+	}
+	iterator end(){
+		return iterator(back_,this);
+	}
+	const_iterator cbegin() const{
+		return const_iterator(front_->next_,this);
+	}
+	const_iterator cend() const{
+		return const_iterator(back_,this);
+	}
 
 	class const_iterator{
 		Node* curr_;
